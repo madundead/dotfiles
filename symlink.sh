776cb6d1e0fcf41144de2477291d4035c075cf67
Konstantin Bukley <kostya.b@matic.com>
2021-04-08 20:32:46 +0300

    planck: starting over

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 for dir in $(ls -d */); do stow -Rv ${dir}; done
+
+# Link planck config to QMK (prime)
+ln -s /Users/madundead/Development/dotfiles/planck/keymap.c  /Users/madundead/Development/qmk/keyboards/planck/keymaps/madundead/keymap.c
